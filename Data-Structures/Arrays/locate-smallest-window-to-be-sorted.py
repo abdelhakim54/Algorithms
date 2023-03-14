7ebe2b8857006aee77ca0f2e85d0d1caba90b1f9
abdelhakim54 <elouakouakabdelhakim@gmail.com>
2023-03-14 11:16:47 +0100

    define the problem

---
+++ Data-Structures/Arrays/locate-smallest-window-to-be-sorted.py
@@ -0,0 +1,6 @@
+"""Problem:
+    Given an array of integers that are out of order, determine the bounds of the smallest 
+    window that must be sorted in order for the entire array to be sorted. 
+   Example, 
+    given [ 3 , 7 , 5 , 6 , 9] , you should return ( 1 , 3 ) . 
+"""
