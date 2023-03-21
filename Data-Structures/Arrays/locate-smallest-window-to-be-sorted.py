9b7a848eefeb01feaafac51e09e557038fa480d5
abdelhakim54 <elouakouakabdelhakim@gmail.com>
2023-03-21 12:00:42 +0000

    solve the locate smallest window problem using somehow a brute force alg

--- Data-Structures/Arrays/locate-smallest-window-to-be-sorted.py
+++ Data-Structures/Arrays/locate-smallest-window-to-be-sorted.py
@@ -4,3 +4,20 @@
    Example, 
     given [ 3 , 7 , 5 , 6 , 9] , you should return ( 1 , 3 ) . 
 """
+
+#This solution is O(nlogn) since the sorted function in python uses the Timsort algorithm
+
+def locate_smallest_window(arr):
+    inf_bound = 0
+    sup_bound = 100000000000
+    sorted_array = sorted(arr)
+
+    for i in range(len(arr)):
+        if arr[i] != sorted_array[i] and inf_bound == 0:
+            inf_bound = i;
+        elif arr[i] != sorted_array[i] :
+            sup_bound = i
+
+    return (inf_bound, sup_bound)
+
+print(locate_smallest_window([3, 7, 5, 6, 9]))
