889131bcfe6a748371418a5c3ecb944aa4fd705c
abdelhakim54 <elouakouakabdelhakim@gmail.com>
2023-03-22 15:24:41 +0000

    solve the max sub-array-sum using a brute force algorithm

--- Data-Structures/Arrays/max-sub-array.py
+++ Data-Structures/Arrays/max-sub-array.py
@@ -6,3 +6,16 @@
     *  Given the array [ -5, -1, -8, -9], the maximum sum would be 0, since we would choose not to 
     take any elements.
 """
+
+def max_sub_array(arr):
+    max_sum = 0
+    temp_sum = 0
+    for i in range(len(arr)):
+        temp_sum = 0
+        for j in range(i, len(arr)):
+            temp_sum+=arr[j]
+            if(temp_sum > max_sum):
+                max_sum = temp_sum
+    return max_sum
+
+print(max_sub_array([ -5, -1, -8, -9]))
